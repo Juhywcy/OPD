26b4f6f427df2e535afc45a2deafa8673933b84f
Juhy <juhywcy@gmail.com>
2026-05-07 21:35:13 +0800

    y

---
+++ install.sh
@@ -0,0 +1,24 @@
+pip install "sglang[all]==0.5.2" --no-cache-dir && pip install torch-memory-saver --no-cache-dir
+
+pip install --no-cache-dir "vllm==0.11.0"
+
+pip install "transformers[hf_xet]>=4.51.0" accelerate datasets peft hf-transfer \
+    "numpy<2.0.0" "pyarrow>=15.0.0" pandas "tensordict>=0.8.0,<=0.10.0,!=0.9.0" torchdata \
+    ray[default] codetiming hydra-core pylatexenc qwen-vl-utils wandb dill pybind11 liger-kernel mathruler \
+    pytest py-spy pre-commit ruff tensorboard 
+
+pip install "nvidia-ml-py>=12.560.30" "fastapi[standard]>=0.115.0" "optree>=0.13.0" "pydantic>=2.9" "grpcio>=1.62.1"
+
+mkdir third_party
+
+export HF_ENDPOINT=https://hf-mirror.com
+huggingface-cli download --resume-download Juhywcy/verl-whl --local-dir third_party
+
+pip install --no-cache-dir third_party/flash_attn-2.8.1+cu12torch2.8cxx11abiFALSE-cp312-cp312-linux_x86_64.whl
+pip install --no-cache-dir flashinfer-python==0.3.1
+
+pip install opencv-python
+pip install opencv-fixer && \
+    python -c "from opencv_fixer import AutoFix; AutoFix()"
+
+pip install swanlab matplotlib
